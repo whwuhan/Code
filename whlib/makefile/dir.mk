4bc744d28c888f2f72432be385d97acfe864b2e6
unknown <695549978@qq.com>
2020-08-09 17:48:46 +0800

    搭建目录结构，并做测试

---
+++ whlib/makefile/dir.mk
@@ -0,0 +1,8 @@
+#	目录变量定义
+CC := g++
+INCLUDE_DIR := ../include/#头文件目录
+SRC_DIR := ../src/#源代码目录
+DES_DIR := ../bin/#最后生成结果存放目录
+MAIN_DIR := ../src/main/#主函数目录
+UTILS_DIR := ../src/utils/#utils目录
+BASIC_DIR := ../src/basic/#basic目录
---
+++ whlib/makefile/test.mk
@@ -0,0 +1,8 @@
+include dir.mk
+
+
+test : 
+	$(CC) $(MAIN_DIR)main.cpp $(UTILS_DIR)string_lib.cpp -o $(DES_DIR)test
+
+
+
