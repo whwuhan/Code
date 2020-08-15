de0dfd827f4c72cde8cb2b5330185796531b8578
unknown <695549978@qq.com>
2020-08-15 15:47:08 +0800

    改善效率

--- whlib/src/utils/io.cpp
+++ whlib/src/utils/io.cpp
@@ -21,15 +21,38 @@ void wh::utils::load_point_cloud_obj(const string file_name,struct Point_cloud* 
     //读入文件
     string line;
     vector<string> line_split;
+    unsigned int count = 0;//obj文件点的数量
+
     while(getline(data_source, line))
     {   
         if(line[0] == 'v')
         {   
             line_split = split(line, " ");
+            count++;
+            //cout << count << endl;
+            /** resize()效率太低，改成先遍历点的个数，再resize()一次
             point_cloud_ptr->conservative_resize((++point_cloud_ptr->size), 3);
             point_cloud_ptr->points(point_cloud_ptr->size-1, 0) = atof(line_split[1].c_str());
             point_cloud_ptr->points(point_cloud_ptr->size-1, 1) = atof(line_split[2].c_str());
             point_cloud_ptr->points(point_cloud_ptr->size-1, 2) = atof(line_split[3].c_str());
+            */
+        }
+    }
+
+    data_source.clear();//先要clear()才能回到文件头
+    data_source.seekg(0, ios::beg);
+    point_cloud_ptr->resize(count, 3);
+    count = 0;
+
+    while(getline(data_source, line))
+    {   
+        if(line[0] == 'v')
+        {   
+            line_split = split(line, " ");
+            point_cloud_ptr->points(count, 0) = atof(line_split[1].c_str());
+            point_cloud_ptr->points(count, 1) = atof(line_split[2].c_str());
+            point_cloud_ptr->points(count, 2) = atof(line_split[3].c_str());
+            count++;
         }
     }
     data_source.close();
--- whlib/test/main/test_io_obj.cpp
+++ whlib/test/main/test_io_obj.cpp
@@ -8,8 +8,10 @@ using namespace wh::utils;
 int main()
 {
     Point_cloud point_cloud;
-    load_point_cloud_obj("test.obj", &point_cloud);
-    cout << point_cloud << endl;
+    cout << "start" << endl;
+    load_point_cloud_obj("C:/Users/Administrator/Desktop/trunk_mve_scan_1.obj", &point_cloud);
+    //cout << point_cloud << endl;
     cout << point_cloud.size << endl;
     cout << point_cloud.get_geometric_center() << endl;
+    cout << "end" << endl;
 }
