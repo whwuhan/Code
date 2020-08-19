d437d46508d7cb49aa96ab6e56100b1cc63560d6
unknown <695549978@qq.com>
2020-08-19 18:51:01 +0800

    更改配置文件

--- whlib/test/makefile/test_point3d.mk
+++ whlib/test/makefile/test_point3d.mk
@@ -1,4 +1,4 @@
-include config/config.mk
+include config/*.mk
 
 
 test : 
--- whlib/test/makefile/test_point_cloud.mk
+++ whlib/test/makefile/test_point_cloud.mk
@@ -1,4 +1,4 @@
-include config/config.mk
+include config/*.mk
 
 test_point_cloud : 
 	$(CC) $(TEST_MAIN_DIR)test_point_cloud.cpp $(BASIC_DIR)point_cloud.cpp $(BASIC_DIR)point3d.cpp -o $(TEST_DES_DIR)test_point_cloud
