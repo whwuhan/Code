3f374b9180a4ccfd6cb3a3b80a3d2ca00a420307
unknown <695549978@qq.com>
2020-08-20 17:49:22 +0800

    更改目录结构，将新建utils下io目录

--- whlib/test/makefile/test_io_obj.mk
+++ whlib/test/makefile/test_io_obj.mk
@@ -1,7 +1,7 @@
 include config/*.mk
 
 test_io_obj :
-	$(CC) $(TEST_MAIN_DIR)/test_io_obj.cpp $(IO_SRC) $(STRING_LIB_SRC) $(POINT_CLOUD_SRC) $(POINT3D_SRC) -o $(TEST_DES_DIR)/test_io_obj
+	$(CC) $(TEST_MAIN_DIR)/test_io_obj.cpp $(IO_OBJ_SRC) $(STRING_LIB_SRC) $(POINT_CLOUD_SRC) $(POINT3D_SRC) -o $(TEST_DES_DIR)/test_io_obj
 
 .PHONY : clean
 clean :  
