82a9d3a38c54f09596709984a28b33e208001f35
unknown <695549978@qq.com>
2020-08-21 18:35:56 +0800

    调整目录

--- whlib/makefile/config/win_dir.mk
+++
@@ -1,8 +0,0 @@
-#	windows下目录变量定义
-CC := g++
-INCLUDE_DIR := ../include/#头文件目录
-SRC_DIR := ../src/#源代码目录
-DES_DIR := ../bin/win/#最后生成结果存放目录
-MAIN_DIR := ../src/main/#主函数目录
-UTILS_DIR := ../src/utils/#utils目录
-BASIC_DIR := ../src/basic/#basic目录
