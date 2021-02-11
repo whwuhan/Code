0a505e5c18bd49500fe8d868a4e243b87dde7183
whwuhan <695549978@qq.com>
2021-02-11 23:28:22 +0800

    日常更新

--- opengl/shader/43/43.prefilter.fs.glsl
+++ opengl/shader/43/43.prefilter.fs.glsl
@@ -1,4 +1,10 @@
 #version 330 core
+/**
+
+    计算prefilterMap 跟环境光diffuse部分的irradianceMap类似，但是将roughness考虑在内，等级越高的mipmap，roughness越高
+    也就是环境光镜面反射的第一部分，将渲染好的环境光先放到一个cubemap中
+
+*/
 out vec4 FragColor;
 in vec3 WorldPos;
 
@@ -6,9 +12,8 @@ uniform samplerCube environmentMap;//环境立方体贴图
 uniform float roughness;// 粗糙程度
 
 const float PI = 3.14159265359;
-
-const float PI = 3.14159265359;
 // ----------------------------------------------------------------------------
+// 法线分布方程
 float DistributionGGX(vec3 N, vec3 H, float roughness)
 {
     float a = roughness*roughness;
@@ -21,4 +26,97 @@ float DistributionGGX(vec3 N, vec3 H, float roughness)
     denom = PI * denom * denom;
 
     return nom / denom;
+}
+
+
+// ----------------------------------------------------------------------------
+// http://holger.dammertz.org/stuff/notes_HammersleyOnHemisphere.html
+// efficient VanDerCorpus calculation.
+// 生成low-discrepancy sequance 用于Monte Carlo积分采样
+float RadicalInverse_VdC(uint bits) 
+{
+     bits = (bits << 16u) | (bits >> 16u);
+     bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
+     bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
+     bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
+     bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
+     return float(bits) * 2.3283064365386963e-10; // / 0x100000000
+}
+// ----------------------------------------------------------------------------
+vec2 Hammersley(uint i, uint N)
+{
+    return vec2(float(i) / float(N), RadicalInverse_VdC(i));
+}
+// ----------------------------------------------------------------------------
+// 重要性采样importance sampling 在采样点的附近采样（相当于在采样向量上面加上一个bias）
+// 跟BRDF方程没有任何关系，就只是单纯的采样，单纯的在half-way vector附近根据roughness进行随机均匀采样
+vec3 ImportanceSampleGGX(vec2 Xi, vec3 N, float roughness)
+{
+	float a = roughness*roughness;
+	
+	float phi = 2.0 * PI * Xi.x;
+	float cosTheta = sqrt((1.0 - Xi.y) / (1.0 + (a*a - 1.0) * Xi.y));//这里roughness为0的时候，cosTheta = 1
+	float sinTheta = sqrt(1.0 - cosTheta*cosTheta);
+	
+	// from spherical coordinates to cartesian coordinates - halfway vector
+    // 注意坐标系的方向，z轴是朝上的
+	vec3 H;
+	H.x = cos(phi) * sinTheta;
+	H.y = sin(phi) * sinTheta;
+	H.z = cosTheta;
+	
+	// from tangent-space H vector to world-space sample vector
+	vec3 up          = abs(N.z) < 0.999 ? vec3(0.0, 0.0, 1.0) : vec3(1.0, 0.0, 0.0);
+	vec3 tangent   = normalize(cross(up, N));
+	vec3 bitangent = cross(N, tangent);
+	
+	vec3 sampleVec = tangent * H.x + bitangent * H.y + N * H.z;
+	return normalize(sampleVec);
+}
+
+
+
+
+void main()
+{
+    vec3 N = normalize(WorldPos);
+
+    // make the simplyfying assumption that V equals R equals the normal 
+    // 视线恰好就在反射方向上面
+    vec3 R = N;
+    vec3 V = R;
+
+    const uint SAMPLE_COUNT = 1024u;
+    vec3 prefilteredColor = vec3(0.0);
+    float totalWeight = 0.0;
+
+    for(uint i = 0u; i < SAMPLE_COUNT; ++i)
+    {
+        // generates a sample vector that's biased towards the preferred alignment direction (importance sampling).
+        vec2 Xi = Hammersley(i, SAMPLE_COUNT);
+        vec3 H = ImportanceSampleGGX(Xi, N, roughness); //half-way vector
+        vec3 L = normalize(2.0 * dot(V, H) * H - V);
+
+        float NdotL = max(dot(N, L), 0.0);
+        if(NdotL > 0.0)//保证光线是从平面的上方射入，之后反射到视线中
+        {
+            // sample from the environment's mip level based on roughness/pdf
+            float D = DistributionGGX(N, H, roughness);//近似的是微表面的法线分布，这里用于限制散射量
+            float NdotH = max(dot(N, H), 0.0);
+            float HdotV = max(dot(H, V), 0.0);
+            float pdf = D * NdotH / (4.0 * HdotV) + 0.0001;
+
+            float resolution = 512.0;//resolution of source cubemap (per face)
+            float saTexel = 4.0 * PI / (6.0  * resolution * resolution);
+            float saSample = 1.0 / (float(SAMPLE_COUNT)* pdf + 0.0001);
+
+            float mipLevel = roughness == 0.0 ? 0.0 : 0.5 * log2(saSample / saTexel);
+
+            prefilteredColor += textureLod(environmentMap, L, mipLevel).rgb * NdotL;
+            totalWeight += NdotL;
+        }
+    }
+
+    prefilteredColor = prefilteredColor / totalWeight;
+    FragColor = vec4(prefilteredColor, 1.0);
 }
