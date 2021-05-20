0fb95e56fa77716e39ed8c164abfee08a1fb0a6e
wuhan <695549978@qq.com>
2021-05-20 20:12:21 +0800

    update

--- Cpp/Reflection/main.cpp
+++ Cpp/Reflection/main.cpp
@@ -13,56 +13,74 @@ using namespace std;
 //-------------------反射器实现-------------------
 
 //所有需要反射的类需要继承ReflectObject
-class ReflectObject{
+class ReflectObject
+{
 public:
     virtual ~ReflectObject(){}
 };
 
 //对象工厂抽象类（也就是基类），用来生成对象实例
-class ObjectFactory{
+class ObjectFactory
+{
 public:
     //构造函数
-    ObjectFactory(){
+    ObjectFactory()
+    {
         cout << "ObjectFactory()" << endl;
     }
+
     //虚析构函数
-    virtual ~ObjectFactory(){
+    virtual ~ObjectFactory()
+    {
         cout << "~ObjectFactory()" << endl;
     }
+    
     //纯虚函数，获得实例的接口
     virtual ReflectObject* newInstance() = 0;
 };
 
 //反射器，用来管理（对象名，对象工厂）的映射关系
-class Reflector{
+class Reflector
+{
 public:
     Reflector(){}
 
     //析构函数，释放new的内存
-    ~Reflector(){
-        for(auto it = objectFactories.begin(); it != objectFactories.end(); it++){
+    ~Reflector()
+    {
+        for(auto it = objectFactories.begin(); it != objectFactories.end(); it++)
+        {
             delete it->second;
         }
         objectFactories.clear();
     }
 
     //将需要反射的类的对象工厂注册到Reflector中
-    void registerFactory(const string& className, ObjectFactory* of){
+    void registerFactory(const string& className, ObjectFactory* of)
+    {
         auto it = objectFactories.find(className);
-        if(it != objectFactories.end()){
+        if(it != objectFactories.end())
+        {
             cout << "该类的生产工厂已经存在..." << endl;
-        }else{
+        }
+        else
+        {
             objectFactories[className] = of;
         }
     }
 
     //通过类名获取对象实例
-    ReflectObject* getNewInstance(const string className){
+    ReflectObject* getNewInstance(const string className)
+    {
         auto it = objectFactories.find(className); 
         //有对应的工厂（已经注册）
-        if(it != objectFactories.end()){
+        if(it != objectFactories.end())
+        {
             return it->second->newInstance();
-        }else{//没有注册返回空指针
+        }
+        else
+        {
+            //没有注册返回空指针
             cout << "还没有注册生产工厂" << endl;
             return nullptr;
         }
@@ -73,33 +91,42 @@ private:
 };
 
 //生成反射器的函数，保证全局只有一个反射器
-Reflector& reflector(){
-    static Reflector reflector;//static只会在程序初始化时生成反射器
+Reflector& reflector()
+{
+    static Reflector reflector; //static只会在程序初始化时生成反射器
     return reflector;
 }
 
-//利用宏定义将需要反射的类，注册到Reflector的objectFactories中
-//宏定义中##表示拼接，#表示将后面的参数变成字符串
+// 利用宏定义将需要反射的类，注册到Reflector的objectFactories中
+// 宏定义中##表示拼接，
+// #表示将后面的参数变成字符串 
+// #@x表示给x加上单引号，返回一个const char
 #define REFLECT(className) \
-class ObjectFactory_##className : public ObjectFactory{ \
+class ObjectFactory_##className : public ObjectFactory \
+{ \
 public: \
     /* 对应工厂的构造函数 */ \
-    ObjectFactory_##className(){ \
+    ObjectFactory_##className() \
+    { \
         cout << "ObjectFactory_" << #className << "()" << endl; \
     } \
     /* 对应工厂的析构函 */ \
-    virtual ~ObjectFactory_##className(){ \
+    virtual ~ObjectFactory_##className() \
+    { \
         cout << "~ObjectFactory_" << #className << "()" << endl; \
     } \
     /* 重写ObjectFactory中的 newInstance()函数 */ \
-    ReflectObject* newInstance(){ \
+    ReflectObject* newInstance() \
+    { \
         return new className(); \
     } \
 }; \
 /* 注册类的生产工厂 */ \
-class Register_##className{ \
+class Register_##className \
+{ \
 public: \
-    Register_##className(){ \
+    Register_##className() \
+    { \
         reflector().registerFactory(#className, new ObjectFactory_##className()); \
     } \
 }; \
@@ -110,40 +137,54 @@ Register_##className register_##className; \
 
 //添加通过类名获取对象实例的模板方法
 template<typename T>
-T* getNewInstance(const string& className){
+T* getNewInstance(const string& className)
+{
     return dynamic_cast<T*>(reflector().getNewInstance(className));
 }
 
 //Person基类继承ReflectObject
-class Person : public ReflectObject{
+class Person : public ReflectObject
+{
 public:
-    Person(){
+    Person()
+    {
         cout << "Person()" << endl;
     }
-    virtual ~Person(){
+
+    virtual ~Person()
+    {
         cout << "~Person()" << endl;
     }
-    virtual void show(){
+
+    virtual void show()
+    {
         cout << "Hello, I'm Person" << endl;
     }
 };
 
-class Wuhan : public Person{
+class Wuhan : public Person
+{
 public:
-    Wuhan(){
+    Wuhan()
+    {
         cout << "Wuhan()" << endl;
     }
-    virtual ~Wuhan(){
+
+    virtual ~Wuhan()
+    {
         cout << "~Wuhan()" << endl;
     }
-    virtual void show(){
+
+    virtual void show()
+    {
         cout << "Hello, I'm Wuhan" << endl;
     }
 };
 //将Wuhan注册
 REFLECT(Wuhan)
 
-int main(){
+int main()
+{
     Person* wuhan = getNewInstance<Person>("Wuhan");
     wuhan->show();
     delete wuhan;
---
+++ Cpp/VariableParameter/main.cpp
@@ -0,0 +1,49 @@
+/**
+ * 可变参数练习
+*/
+
+#include <iostream>
+#include <cstdarg> //支持可变参数的头文件 包含va_arg va_start va_end va_list
+using namespace std;
+
+// 求和函数
+int add(int paramNumber, ...) // paramNumber 可变参数的数量
+{
+    va_list argPtr; // 用于接收可变参数，现在还没有接收
+    int sum = 0;
+    int nArgValue;
+    /**
+     * va_start(args,paramN) 令对可变函数参数的访问
+     * 第一个参数为va_list类型的变量，va_list接收所有的可变参数
+     * 第二个参数为"..."前最后一个参数，也就是可变参数前的，最后一个参数
+     * 将args初始化为指向可变参数列表第一个参数
+    */
+    va_start(argPtr, paramNumber); // 接收可变参数
+    while(paramNumber > 0)
+    {
+        /**
+         * va_arg(args,type)  访问下一个可变函数参数。
+         * 第一个参数为va_list类型的变量
+         * 第二个参数是返回值的类型
+         * 调用va_arg获取当前的参数，并自动更新指向下一个可变参数
+        */
+        nArgValue = va_arg(argPtr, int);
+        sum += nArgValue;
+        paramNumber--;
+        cout << nArgValue << endl;
+    }
+
+    /**
+     * va_end(args)  结束可变参数函数的遍历
+     * 释放va_arg变量
+    */
+   va_end(argPtr);
+   return sum;
+}
+
+
+int main()
+{
+    int result = add(3, 3, 4, 5);
+    cout << result << endl;
+}
