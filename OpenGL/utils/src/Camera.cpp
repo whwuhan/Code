6f3787b9d985a123f5db2a105399c402e6b6f176
whwuhan <695549978@qq.com>
2021-06-05 15:05:45 +0800

    update

--- OpenGL/utils/src/Camera.cpp
+++ OpenGL/utils/src/Camera.cpp
@@ -8,7 +8,8 @@ Camera::Camera(glm::vec3 position,glm::vec3 up,float yaw,float pitch):
 Front(glm::vec3(0.0f,0.0f,-1.0f)),
 MovementSpeed(SPEED),
 MouseSensitivity(SENSITIVITY),
-Zoom(ZOOM){
+Zoom(ZOOM)
+{
     Position = position;
     WorldUp = up;
     Yaw = yaw;
@@ -21,19 +22,22 @@ Zoom(ZOOM){
     updateCameraVectors();
 }
 
-Camera::Camera(
-float posX,
-float posY,
-float posZ,
-float upX,
-float upY,
-float upZ,
-float yaw,
-float pitch):
+Camera::Camera
+(
+    float posX,
+    float posY,
+    float posZ,
+    float upX,
+    float upY,
+    float upZ,
+    float yaw,
+    float pitch
+):
 Front(glm::vec3(0.0f, 0.0f, -1.0f)),
 MovementSpeed(SPEED),
 MouseSensitivity(SENSITIVITY),
-Zoom(ZOOM){
+Zoom(ZOOM)
+{
     Position = glm::vec3(posX, posY, posZ);
     WorldUp = glm::vec3(upX, upY, upZ);
     Yaw = yaw;
@@ -42,37 +46,45 @@ Zoom(ZOOM){
 }
 
 //获取观察矩阵，也就是loocAt矩阵
-glm::mat4 Camera::GetViewMatrix(){
+glm::mat4 Camera::GetViewMatrix()
+{
     return glm::lookAt(Position, Position + Front, Up);
 }
 
 //响应键盘
-void Camera::ProcessKeyboard(Camera_Movement direction,float deltaTime){
+void Camera::ProcessKeyboard(Camera_Movement direction,float deltaTime)
+{
     float velocity = MovementSpeed * deltaTime;
     //向前
-    if(direction == FORWARD){
+    if(direction == FORWARD)
+    {
         Position += Front * velocity;
     }
     //向后
-    if(direction == BACKWARD){
+    if(direction == BACKWARD)
+    {
         Position -= Front * velocity;
     }
     //向左
-    if(direction == LEFT){
+    if(direction == LEFT)
+    {
         Position -= Right * velocity;
     }
     //向右
-    if(direction == RIGHT){
+    if(direction == RIGHT)
+    {
         Position += Right * velocity;
     }
     //向上（世界上方，不是相机上方）
-    if(direction == UPWARD){
+    if(direction == UPWARD)
+    {
         Position += WorldUp * velocity;
     }
 }
 
 //响应鼠标
-void Camera::ProcessMouseMovement(float xoffset,float yoffset,GLboolean constrainPitch){
+void Camera::ProcessMouseMovement(float xoffset,float yoffset,GLboolean constrainPitch)
+{
     //xy的偏移量
     xoffset *= MouseSensitivity;
     yoffset *= MouseSensitivity;
@@ -81,11 +93,14 @@ void Camera::ProcessMouseMovement(float xoffset,float yoffset,GLboolean constrai
     Pitch += yoffset;//俯仰角
 
     //判断是否锁定俯仰角
-    if(constrainPitch){
-        if(Pitch > 89.0f){
+    if(constrainPitch)
+    {
+        if(Pitch > 89.0f)
+        {
             Pitch = 89.0f;
         }
-        if(Pitch < -89.0f){
+        if(Pitch < -89.0f)
+        {
             Pitch = -89.0f;
         }
     }
@@ -94,7 +109,8 @@ void Camera::ProcessMouseMovement(float xoffset,float yoffset,GLboolean constrai
 }
 
 //响应滚轮，yoffset表示竖直滚动的大小
-void Camera::ProcessMouseScroll(float yoffset){
+void Camera::ProcessMouseScroll(float yoffset)
+{
     Zoom -= (float)yoffset;//Zoom就是视角fov
     if(Zoom < 1.0f)
     {
@@ -107,7 +123,8 @@ void Camera::ProcessMouseScroll(float yoffset){
 }
 
 //计算相机前向角
-void Camera::updateCameraVectors(){
+void Camera::updateCameraVectors()
+{
     glm::vec3 front;
     front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
     front.y = sin(glm::radians(Pitch));
@@ -119,7 +136,8 @@ void Camera::updateCameraVectors(){
 }
 
 //显示相机信息
-void Camera::showInf(){
+void Camera::showInf()
+{
     std::cout<<"-----------------------------------------"<<std::endl;
     std::cout<<"Camera information:"<<std::endl;
     std::cout<<"Position:["<<Position.x<<","<<Position.y<<","<<Position.z<<"]"<<std::endl;
